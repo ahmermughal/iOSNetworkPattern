e0e3602a6373ee47b65dd31d682d0ca12ea0a80e
ahmermughal <a2mughal143@gmail.com>
2022-07-20 17:17:45 +0500

    Initial Commit

---
+++ NetworkDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkDemo
+//
+//  Created by Ahmer Mughal on 7/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
